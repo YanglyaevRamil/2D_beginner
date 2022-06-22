f4f16bffce5fa1120d1103d5555c635696303581
YanglyaevRamil <32882454+YanglyaevRamil@users.noreply.github.com>
2022-06-22 18:44:58 +0300

    Add files via upload

---
+++ Assets/Scripts/View/CoinView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CoinView : MonoBehaviour, ICoinProvider
+{
+    [SerializeField]
+    private int _price;
+
+    public int Price { get { return _price; } set { _price = value; } }
+}
